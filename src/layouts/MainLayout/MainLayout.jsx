f10f08b70bd7fe880a6001e8e7b4f534a2ac748d
skmajumder <skmajumder.cse@gmail.com>
2023-06-08 18:50:10 +0600

    Router setup

---
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PageTitle from "../../components/PageTitle/PageTitle";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <>
+      <PageTitle pageTitle={"Home"} />
+      <Header />
+      <main className="min-h-[calc(100vh-120px)]">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
