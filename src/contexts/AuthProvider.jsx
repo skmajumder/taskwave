38475a167107a2d618a3563f2d0b8cec66c37a50
skmajumder <skmajumder.cse@gmail.com>
2023-06-08 19:16:16 +0600

    Auth provider add

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,13 +1,85 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { app } from "../firebase/firebase.config";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  /**
+   * * Authentication (Email Authentication & Google Authentication)
+   */
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+
+  /**
+   * * Create a new user using Email & password (Register)
+   */
+  const signUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  /**
+   * * User login user using Email & password (Login)
+   */
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  /**
+   * * User Authentication using Google Account Authentication
+   */
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  /**
+   * * User Logout
+   */
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  /**
+   * * Reset user password who forget their passwords
+   */
+  const resetPassword = (email) => {
+    setLoading(true);
+    sendPasswordResetEmail(auth, email);
+  };
+
+  /**
+   * * Observe the user current status. Check user is login or logged out
+   */
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+
   const authInfo = {
     user,
     loading,
+    signUp,
+    signIn,
+    googleSignIn,
+    logout,
+    resetPassword,
   };
 
   return (
